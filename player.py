637993ee4db071de1ae710fe5f0f0ab509a456f3
Thredo <pirizleonardod@gmail.com>
2025-01-15 22:14:49 -0300

    Nerfed the ships gun so now it cant shoot constantly

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__ (self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
 
     # in the player class
@@ -26,7 +27,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.timer -= dt
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -43,6 +44,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt    
 
     def shoot(self):
-        velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x,self.position.y, SHOT_RADIUS, velocity)
+        if self.timer <= 0:
+            velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            shot = Shot(self.position.x,self.position.y, SHOT_RADIUS, velocity)
+            self.timer = PLAYER_SHOOT_COOLDOWN
     
